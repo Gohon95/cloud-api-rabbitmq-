aa33200388e03a8c761b25d3e97e524beea95f88
Axel <axel@40-60studio.com>
2023-01-18 22:39:30 +0100

    add amqp

--- src/controllers/command.controller.js
+++ src/controllers/command.controller.js
@@ -3,11 +3,12 @@
  * Author: Quarante Soixante Nantes
  *
  * Modified By: Axel GONON
- * Last Modified: 18/01/2023 08:33:17
+ * Last Modified: 18/01/2023 22:35:17
  */
 
 const catchAsync = require("../utils/catchAsync");
 const dbService = require("../services/db.service");
+const amqp = require("amqplib/callback_api");
 
 // import des classes models
 const Command = require("../models/command.model");
@@ -20,11 +21,60 @@ const Command = require("../models/command.model");
  */
 const creerCommand = catchAsync(async (req, res) => {
   let dataToCreate = req.body;
+  flag: "commande en cours";
 
   let creationCommand = await dbService.create(Command, dataToCreate);
 
   res.send(creationCommand);
+
+  // Step 1 : Create Connection
+  amqp.connect("amqp://localhost", (connError, connection) => {
+    if (connError) {
+      throw connError;
+    }
+    // Step 2: Create Channel
+    connection.createChannel((channelError, channel) => {
+      if (channelError) {
+        throw channelError;
+      }
+      // Step 3: Assert Queue
+      const QUEUE = "statut";
+      channel.assertQueue(QUEUE);
+      // Step 4: Send message to queue
+      channel.sendToQueue(QUEUE, Buffer.from("hello your status is available"));
+      console.log(`Message send ${QUEUE}`);
+    });
+  });
 });
+function worker() {
+  amqplib.connect(process.env.AMQP_URL, (err, conn) => {
+    if (err) throw err;
+    console.log("Connection rabbitmq");
+
+    conn.createChannel((err, ch) => {
+      if (err) throw err;
+      console.log("Channel created");
+
+      ch.assertQueue(queue, { durable: true });
+
+      ch.consume(queue, (msg) => {
+        if (msg !== null) {
+          console.log("Message :", msg.content.toString());
+          Commande.findByIdAndUpdate(
+            msg.content.toString(),
+            { flag: "commande terminée" },
+            { upsert: true },
+            () => {
+              if (err) throw err;
+              console.log("Commande updated");
+              worker();
+            }
+          );
+        }
+      });
+    });
+  });
+}
 
 module.exports = {
   creerCommand,
